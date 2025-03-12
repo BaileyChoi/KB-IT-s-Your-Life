709c8b1c885599016d5440896fd61607fbbed561
BaileyChoi <haileycho9981@gmail.com>
2025-03-12 17:21:26 +0900

    문제: 자바 스크립트 비동기처리 문제

---
+++ 02_Node.js/practice/make.js
@@ -0,0 +1,15 @@
+function make1() {
+	//구현
+	console.log("Have a nice day!!");
+}
+make1(); //호출
+
+function make2(num) {
+	console.log(`num = ${num}`);
+}
+make2(100);
+
+function make3(a, b) {
+	console.log(`${a} + ${b} = ${a + b}`);
+}
+make3(25, 36);
