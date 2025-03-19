eb48afe2dca824b42695daa6d398c1207c481e78
BaileyChoi <haileycho9981@gmail.com>
2025-03-19 16:26:08 +0900

    실습: 배열 메서드

---
+++ 02_Node.js/supplement/array/08_concat.js
@@ -0,0 +1,9 @@
+const ar1 = [0, 1, 2];
+const ar2 = [3, 4, 5, 6, 7];
+const ar3 = ["쩜프", "병철이", "새삼이"];
+
+const ar4 = ar1.concat(ar2);
+console.log(ar4);
+
+const ar5 = ar1.concat(ar2, ar3);
+console.log(ar5);
