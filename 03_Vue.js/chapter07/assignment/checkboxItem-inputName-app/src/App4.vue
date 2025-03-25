5b41cb790cfdc89ab4cb86ed30a4849dd14240fe
BaileyChoi <haileychoi9981@gmail.com>
2025-03-25 17:51:39 +0900

    문제: 체크박스아이템, inputName, 투두리스트 리팩토링

---
+++ 03_Vue.js/chapter07/assignment/checkboxItem-inputName-app/src/App4.vue
@@ -0,0 +1,26 @@
+<script>
+import InputName from "./components/InputName.vue";
+
+export default {
+	name: "App",
+	components: {
+		InputName,
+	},
+	data() {
+		return { nameData: "" };
+	},
+	methods: {
+		nameChangedHandler(e) {
+			this.nameData = e.name;
+		},
+	},
+};
+</script>
+
+<template>
+	<div>
+		<InputName @nameChanged="nameChangedHandler" />
+		<br />
+		<h3>정보: {{ nameData }}</h3>
+	</div>
+</template>
---
+++ 03_Vue.js/chapter07/assignment/checkboxItem-inputName-app/src/components/InputName.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+	name: "InputName",
+	data() {
+		return {
+			name: "",
+		};
+	},
+	emits: {
+		nameChanged: (e) => {
+			return e.name && typeof e.name === "string" && e.name.trim().length >= 3
+				? true
+				: false;
+		},
+	},
+};
+</script>
+
+<template>
+	<div>
+		이름: <input type="text" v-model="name" />
+		<button @click="$emit('nameChanged', { name })">이벤트 발신</button>
+	</div>
+</template>
