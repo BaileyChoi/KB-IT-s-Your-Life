ac3c89f41068879a720b8a1921a060f446e53f48
BaileyChoi <haileychoi9981@gmail.com>
2025-03-27 11:09:26 +0900

    실습: TodoList App 리팩토링(Composition API)

---
+++ 03_Vue.js/chapter09/todolist-app-refact/src/components/InputTodo.vue
@@ -0,0 +1,35 @@
+<script>
+import { ref } from "vue";
+export default {
+	name: "InputTodo",
+	setup(props, context) {
+		const todo = ref("");
+		const addTodoHandler = () => {
+			if (todo.value.length >= 3) {
+				context.emit("add-todo", todo.value);
+				todo.value = "";
+			}
+		};
+		return { todo, addTodoHandler };
+	},
+};
+</script>
+
+<template>
+	<div class="row mb-3">
+		<div class="col">
+			<div class="input-group">
+				<input
+					type="text"
+					class="form-control"
+					placeholder="할일을 여기에 입력!"
+					v-model.trim="todo"
+					@keyup.enter="addTodoHandler"
+				/>
+				<span class="btn btn-primary input-group-addon" @click="addTodoHandler"
+					>추가</span
+				>
+			</div>
+		</div>
+	</div>
+</template>
---
+++ 03_Vue.js/chapter09/todolist-app-refact/src/main.js
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/main.css";
+
+import { createApp } from "vue";
+// import App from "./App.vue";
+import App2 from "./App2.vue";
+
+createApp(App2).mount("#app");
