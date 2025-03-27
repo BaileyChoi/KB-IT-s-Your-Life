081eab55136aad5f7211fddb2afc7973df4cca19
BaileyChoi <haileychoi9981@gmail.com>
2025-03-27 09:16:20 +0900

    문제: 갤러리 (수정)

--- 03_Vue.js/practice/Gallery/src/components/GalleryList.vue
+++ 03_Vue.js/practice/Gallery/src/components/GalleryList.vue
@@ -2,26 +2,26 @@
 import GalleryListItem from "./GalleryListItem.vue";
 
 export default {
-  name: "GalleryList",
-  components: {
-    GalleryListItem,
-  },
-  inject: ["images"],
+	name: "GalleryList",
+	components: {
+		GalleryListItem,
+	},
+	inject: ["images"],
 };
 </script>
 
 <template>
-  <ul class="gallery-list">
-    <GalleryListItem v-for="img in images" :key="img.id" :image="img" />
-  </ul>
+	<ul class="gallery-list">
+		<GalleryListItem v-for="img in images" :key="img.id" :image="img" />
+	</ul>
 </template>
 
 <style>
 .gallery-list {
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-  padding: 0;
-  list-style: none;
+	display: flex;
+	justify-content: center;
+	gap: 10px;
+	padding: 0;
+	list-style: none;
 }
 </style>
