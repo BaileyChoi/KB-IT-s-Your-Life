b2f24555874ad6a12597b0b072e3fbf020c6fc7e
BaileyChoi <haileychoi9981@gmail.com>
2025-03-27 16:42:03 +0900

    문제: Composition API

---
+++ 03_Vue.js/chapter09/assignment/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,25 @@
+<script>
+import { ref, watch } from "vue";
+export default {
+	name: "Calc5",
+	setup() {
+		const x = ref(0); // 0으로 초기화
+		const result = ref(0); // 0으로 초기화
+		// 필요한 경우 코드 추가
+		watch(x, (current, old) => {
+			console.log(`x : ${old} --> ${current}`);
+			result.value = current * 2;
+		});
+
+		return { x, result };
+	},
+};
+</script>
+
+<template>
+	<div>
+		x : <input type="text" v-model.number="x" />
+		<br />
+		결과 : {{ result }}
+	</div>
+</template>
