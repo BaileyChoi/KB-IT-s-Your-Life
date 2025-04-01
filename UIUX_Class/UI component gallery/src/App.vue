07e1c41f5f8b301aa13e6c0da6624a2bcec9c925
BaileyChoi <haileychoi9981@gmail.com>
2025-04-01 17:57:15 +0900

    실습: UIUX 특강

---
+++ UIUX_Class/UI component gallery/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import CommonHeader from "./components/CommonHeader.vue";
+import MainGallery from "./components/MainGallery.vue";
+</script>
+
+<template>
+	<div class="container">
+		<CommonHeader />
+		<MainGallery />
+	</div>
+</template>
+
+<style scoped></style>
---
+++ UIUX_Class/UI component gallery/src/components/MainGallery.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseAlert from "./UI/BaseAlert.vue";
+import BaseBadge from "./UI/BaseBadge.vue";
+import BaseButton from "./UI/BaseButton.vue";
+</script>
+
+<template>
+	<!-- 그리드 컨테이너 -->
+	<main class="component-grid">
+		<!-- 컴포넌트 그룹 (그리드 아이템) -->
+		<section class="component-card">
+			<h2>버튼</h2>
+			<BaseButton variant="primary">Primary</BaseButton>
+			<BaseButton variant="outline">Outline</BaseButton>
+			<BaseButton variant="pill">Pill</BaseButton>
+		</section>
+
+		<!-- 알림창 -->
+		<section class="component-card">
+			<h2>알림창</h2>
+			<BaseAlert variant="success">Success</BaseAlert>
+			<BaseAlert variant="warning">Warning</BaseAlert>
+			<BaseAlert variant="error">Error</BaseAlert>
+		</section>
+
+		<!-- 뱃지 -->
+		<section class="component-card">
+			<h2>뱃지</h2>
+			<BaseBadge variant="success">Success</BaseBadge>
+			<BaseBadge variant="number">9</BaseBadge>
+		</section>
+	</main>
+</template>
+
+<style scoped></style>
