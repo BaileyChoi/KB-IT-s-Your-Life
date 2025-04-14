6586ba6439958430bdd718b62b3f92327a95d5e1
BaileyChoi <haileychoi9981@gmail.com>
2025-04-14 13:15:15 +0900

    실습: 자바 시작하기

---
+++ 04_Java/study/src/ch01/sec11/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec11;
+/**
+ * @author Bailey
+ */
+/*
+장제목: 1장 자바 시작하기
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
