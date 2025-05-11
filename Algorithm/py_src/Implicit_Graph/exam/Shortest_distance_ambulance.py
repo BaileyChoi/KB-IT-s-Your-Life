b22d46aa8ff8ba76132d141e63ce051fdb4dde2b
BaileyChoi <haileychoi9981@gmail.com>
2025-05-11 14:43:20 +0900

    알고리즘: cotenossi #32 응급차 최단 거리2

--- Algorithm/py_src/Implicit_Graph/exam/Shortest_distance_ambulance.py
+++ Algorithm/py_src/Implicit_Graph/exam/Shortest_distance_ambulance.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 def solution(city):
     rowLen = len(city)
     colLen = len(city[0])
@@ -7,23 +8,28 @@ def solution(city):
     visited = [[False] * colLen for _ in range(rowLen)]
     dist = [[0] * colLen for _ in range(rowLen)]
 
-    bfs(city, 0 , 0, visited, dist)
+    bfs(city, 0, 0, visited, dist)
 
-    return dist[rowLen - 1][colLen - 1]
+    return (
+        dist[rowLen - 1][colLen - 1]
+        if city[0][0] != 1 and city[rowLen - 1][colLen - 1] != 1
+        else -1
+    )
+
 
 def bfs(city, r, c, visited, dist):
     rowLen = len(city)
     colLen = len(city[0])
 
-    dr = [-1, 1, 0 , 0, -1 , 1, 1, -1]
-    dc = [0, 0, -1, 1, -1, 1, -1 , 1]
+    dr = [-1, 1, 0, 0, -1, 1, 1, -1]
+    dc = [0, 0, -1, 1, -1, 1, -1, 1]
 
     queue = deque()
 
     queue.append([r, c])
     visited[r][c] = True
     dist[r][c] = 1
-    
+
     while queue:
         # 현재 노드 방문
         [curRow, curCol] = queue.popleft()
@@ -33,23 +39,22 @@ def bfs(city, r, c, visited, dist):
             nextRow = curRow + dr[i]
             nextCol = curCol + dc[i]
 
-            if 0 <= nextRow < rowLen and 0 <= nextCol < colLen and city[nextRow][nextCol] == 0:
+            if (
+                0 <= nextRow < rowLen
+                and 0 <= nextCol < colLen
+                and city[nextRow][nextCol] == 0
+            ):
                 if not visited[nextRow][nextCol]:
                     queue.append([nextRow, nextCol])
                     visited[nextRow][nextCol] = True
                     dist[nextRow][nextCol] = dist[curRow][curCol] + 1
 
-# 테스트
-print(solution([
-    [0, 0, 1, 0],
-    [1, 0, 1, 0],
-    [1, 0, 0, 0]
-])) # 4
 
-print(solution([
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 0, 0],
-    [1, 1, 0],
-    [0, 0, 0]
-])) # 5
+# 테스트
+print(solution([[0, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 0]]))  # 4
+
+print(solution([[0, 1, 0], [0, 1, 0], [0, 0, 0], [1, 1, 0], [0, 0, 0]]))  # 5
+
+print(solution([[0, 0, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0], [1, 1, 1, 1]]))  # -1
+
+print(solution([[1, 0, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0], [1, 1, 1, 0]]))  # -1
