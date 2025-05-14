0ea6b0f484903da099bef1202bc725ff1ce61f3e
BaileyChoi <haileychoi9981@gmail.com>
2025-05-14 13:09:12 +0900

    실습: Builder2

---
+++ 04_Java/DesignPattern/src/Builder/Builder2_rombok/Main.java
@@ -0,0 +1,17 @@
+package Builder.Builder2_rombok;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = User.builder()
+                .name("홍길동")
+                .email("hong@scoula.org")
+                .password("123456")
+                .phone("010-1111-22222")
+                .address("서울시")
+                .sex(true)
+                .age(16)
+                .build();
+
+        System.out.println(user);
+    }
+}
