4e0d2146bfe5ff420969a073ede49732fff0e89b
BaileyChoi <haileychoi9981@gmail.com>
2025-06-09 16:23:13 +0900

    실습: Spring Context- Bean 정의

---
+++ 08_Spring/01 Spring Context/ex01/src/main/java/org/scoula/app/Main.java
@@ -0,0 +1,25 @@
+package org.scoula.app;
+
+import org.scoula.config.ProjectConfig;
+import org.scoula.domain.Parrot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        Parrot p = new Parrot();
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+        Parrot p2 = context.getBean(Parrot.class);
+        System.out.println(p == p2 ? "같음" : "다름");
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+    }
+}
