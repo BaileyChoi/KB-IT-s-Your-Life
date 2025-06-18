3a8dcc649a2adf98ab5f8d8d5330df1a15b242c6
BaileyChoi <haileychoi9981@gmail.com>
2025-06-18 10:47:05 +0900

    실습: Swagger

--- 08_Spring/boardapi/src/main/java/org/scoula/config/ServletConfig.java
+++ 08_Spring/boardapi/src/main/java/org/scoula/config/ServletConfig.java
@@ -12,13 +12,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {"org.scoula.exception", "org.scoula.controller", "org.scoula.board.controller"})    // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
+@ComponentScan(basePackages = {"org.scoula.exception", "org.scoula.controller", "org.scoula.board.controller"})
+// Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
 public class ServletConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/resources/**")    // url이 /resources/로 시작하는 모든 경로
                 .addResourceLocations("/resources/");              // webapp/resources/경로로 매핑
+
+        // Swagger UI 리소스를 위한 핸들러 설정
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        // Swagger WebJar 리소스설정
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // Swagger 리소스설정
+        registry.addResourceHandler("/swagger-resources/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/v2/api-docs")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 
     // jsp view resolver 설정
--- 08_Spring/boardapi/src/main/java/org/scoula/config/WebConfig.java
+++ 08_Spring/boardapi/src/main/java/org/scoula/config/WebConfig.java
@@ -20,13 +20,19 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{ServletConfig.class};
+        return new Class[]{ServletConfig.class, SwaggerConfig.class};
     }
 
     // 스프링의 FrontController인 DispatcherServlet이 담당할 Url 매핑 패턴, / : 모든 요청에 대해 매핑
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/" };
+        return new String[]{
+                "/",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/v2/api-docs",
+                "/webjars/**"
+        };
     }
 
     // POST body 문자 인코딩 필터 설정 - UTF-8 설정
@@ -36,7 +42,7 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
 
-        return new Filter[] { characterEncodingFilter };
+        return new Filter[]{characterEncodingFilter};
     }
 
     @Override
