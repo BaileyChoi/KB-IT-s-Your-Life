ff072be1d9b9e647190fcc7f6442654efd6a472d
BaileyChoi <haileychoi9981@gmail.com>
2025-06-26 23:01:58 +0900

    실습: 사용자 인증

--- 08_Spring/ApiServer/src/main/java/org/scoula/controller/SecurityController.java
+++ 08_Spring/ApiServer/src/main/java/org/scoula/controller/SecurityController.java
@@ -3,44 +3,36 @@ package org.scoula.controller;
 import lombok.extern.log4j.Log4j2;
 import org.scoula.security.account.domain.CustomUser;
 import org.scoula.security.account.domain.MemberVO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
-@RequestMapping("/security")
-@Controller
+@RequestMapping("/api/security")
+@RestController
 public class SecurityController {
 
-    @GetMapping("/all")     // 모두 접근 가능
-    public void doAll() {
+    @GetMapping("/all")
+    public ResponseEntity<String> doAll() {
         log.info("do all can access everybody");
+        return ResponseEntity.ok("All can access everybody");
     }
 
-    @GetMapping("/member")  // MEMBER 또는 ADMIN 권한 필요
-    public void doMember(Authentication authentication) {
+    @GetMapping("/member")
+    public ResponseEntity<String> doMember(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
         log.info("username = " + userDetails.getUsername());
+        return ResponseEntity.ok(userDetails.getUsername());
     }
 
-    @GetMapping("/admin")   // ADMIN 권한 필요
-    public void doAdmin(@AuthenticationPrincipal CustomUser customUser) {
+    @GetMapping("/admin")
+    public ResponseEntity<MemberVO> doAdmin(@AuthenticationPrincipal CustomUser customUser) {
         MemberVO member = customUser.getMember();
-
         log.info("username = " + member);
-    }
-
-    @GetMapping("/login")
-    public void login() {
-        log.info("login page");
-    }
-
-    @GetMapping("/logout")
-    public void logout() {
-        log.info("logoutpage");
+        return ResponseEntity.ok(member);
     }
 }
