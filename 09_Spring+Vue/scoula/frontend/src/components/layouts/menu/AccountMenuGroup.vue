9ba97a2b30473f76b8e203092b4318ba6e453509
BaileyChoi <haileychoi9981@gmail.com>
2025-06-30 14:40:57 +0900

    실습: 회원관리-회원가입, 로그인, 로그아웃

--- 09_Spring+Vue/scoula/frontend/src/components/layouts/menu/AccountMenuGroup.vue
+++ 09_Spring+Vue/scoula/frontend/src/components/layouts/menu/AccountMenuGroup.vue
@@ -4,21 +4,24 @@ import MenuItem from "./MenuItem.vue";
 import AccountMenuItem from "./AccountMenuItem.vue";
 import LogoutMenuItem from "./LogoutMenuItem.vue";
 import config from "@/config";
+import { useAuthStore } from "@/stores/auth.js";
 
 const { login, join } = config.accountMenus;
 
-const islogin = computed(() => false); // 임시: 로그인하지 않음
-const username = computed(() => ""); // 임시: 사용자명 없음
+const auth = useAuthStore();
+
+const islogin = computed(() => auth.isLogin);
+const username = computed(() => auth.username);
 </script>
 <template>
-  <ul class="navbar-nav ms-auto">
-    <template v-if="islogin">
-      <AccountMenuItem :username="username" />
-      <LogoutMenuItem />
-    </template>
-    <template v-else>
-      <MenuItem :menu="login" />
-      <MenuItem :menu="join" />
-    </template>
-  </ul>
+	<ul class="navbar-nav ms-auto">
+		<template v-if="islogin">
+			<AccountMenuItem :username="username" />
+			<LogoutMenuItem />
+		</template>
+		<template v-else>
+			<MenuItem :menu="login" />
+			<MenuItem :menu="join" />
+		</template>
+	</ul>
 </template>
